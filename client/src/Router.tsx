5f6050712dadf7604a4346a9e2db61d26c666a79
qudgus21 <qudgus54321@naver.com>
2021-09-27 13:28:35 +0900

    feat: react-transition-group을 사용한 라우팅 애니매이션 추가

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,16 +1,66 @@
-import { FC } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "@/pages/Home";
-import Login from "@/pages/Login";
+import { FC, useEffect, useState } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Switch, Route, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import Dashboard from "@/pages/Dashboard";
+import News from "@/pages/News";
+import Analyze from "@/pages/Analyze";
+import MyPage from "@/pages/MyPage";
+import NotFound from "@/pages/NotFound";
+
+const RouteWrapper = styled.div`
+  flex: 88%;
+  height: 100vh;
+  background-color: #f3f7fc;
+`;
+
+const pages = [
+  { path: "/", order: 1 },
+  { path: "/news", order: 2 },
+  { path: "/analyze", order: 3 },
+  { path: "/mypage", order: 4 },
+];
+
+const findOrder = (path: string) => {
+  const curPageOrder = pages.filter((page) => {
+    return page.path === path;
+  });
+  return curPageOrder[0].order;
+};
 
 const MyRouter: FC = () => {
+  const location = useLocation();
+  const [order, setOrder] = useState<number>(findOrder(location.pathname));
+  const [page, setPage] = useState<string>(location.pathname);
+  const [direction, setDirection] = useState<string>();
+
+  useEffect(() => {
+    let currentOrder = order;
+    let currentPage = page;
+    let newOrder = findOrder(location.pathname);
+    let newPage = location.pathname;
+    if (newPage !== currentPage) {
+      let dir = currentOrder < newOrder ? "up" : "down";
+      setOrder(newOrder);
+      setPage(newPage);
+      setDirection(dir);
+    }
+  });
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" exact component={Login} />
-      </Switch>
-    </BrowserRouter>
+    <RouteWrapper>
+      <TransitionGroup className={`transition-group ${direction}`}>
+        <CSSTransition key={location.pathname} classNames="fade" timeout={400}>
+          <Switch location={location}>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/news" exact component={News} />
+            <Route path="/analyze" exact component={Analyze} />
+            <Route path="/mypage" exact component={MyPage} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </RouteWrapper>
   );
 };
 
