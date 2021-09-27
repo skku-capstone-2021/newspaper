3b62a79a221a5189dc53ccbe4453e3f01a3d3bc3
qudgus21 <qudgus54321@naver.com>
2021-09-27 13:25:54 +0900

    chore: svg 파일 추가 및 이미지 index파일 생성

---
+++ client/src/assets/index.tsx
@@ -0,0 +1,9 @@
+// sidebar
+import Analyze from "./analyze.svg";
+import Calendar from "./calendar.svg";
+import Dashboard from "./dashboard.svg";
+import Mypage from "./mypage.svg";
+import News from "./news.svg";
+import Logo from "./logo.svg";
+
+export { Analyze, Calendar, Dashboard, Mypage, News, Logo };
