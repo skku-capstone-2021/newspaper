94162d462edfc3cd829ea82d537f613337238365
qudgus21 <qudgus54321@naver.com>
2021-10-20 02:30:38 +0900

    chore: build

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -5,31 +5,7 @@ module.exports = {
   mode: "production",
 
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        loader: "url-loader",
-        options: {
-          limit: 8 * 1024,
-        },
-      },
-      {
-        test: /\.svg$/i,
-        loader: "svg-url-loader",
-        options: {
-          limit: 8 * 1024,
-        },
-      },
-      {
-        test: /\.(svg|png|jpe?g|gif)$/i,
-        loader: "image-webpack-loader",
-        enforce: "pre",
-      },
-    ],
+    rules: [],
   },
 
   performance: {
@@ -37,7 +13,6 @@ module.exports = {
     maxEntrypointSize: 512000,
     maxAssetSize: 512000,
   },
-  devtool: "inline-source-map",
 
   plugins: [new MiniCssExtractPlugin(), new CleanWebpackPlugin()],
 };
